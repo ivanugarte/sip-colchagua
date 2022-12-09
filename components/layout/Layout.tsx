565705de9b6fd9c95a16f0ab7c8ee4ed42be527f
ivanugarte <ivanugartem@gmail.com>
2022-12-08 22:32:04 -0300

    limpio para build

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,17 +1,15 @@
 import Cards from "../cards/Cards"
-import Footer from "./footer/Footer"
 import Nav from "./nav/Nav"
 import Whatsapp from "./whatsapp/Whatsapp"
 
 
-export const Layout = ({children})=>{
+export const Layout = ( )=>{
   return (
     <>
       <Whatsapp/>
       <Nav />
       <Cards />
-      {children}
-      <Footer />
+      {/* {children} */}
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import About from "../components/about/About";
 import Advantage from "../components/advantage/Advantage";
 import PanelSip from "../components/panelSip/Panelsip";
 import { Layout } from "../components/layout/Layout";
-import Items from "../components/slide/Slider";
-import Slider from "../components/slide/Slider";
 
 export default function Home() {
 
